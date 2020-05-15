33cde5943bc16ca54ddb9a50cd7351cd18a713b0
Somya-5353 <somya.pandey@gonuclei.com>
2020-05-15 16:35:31 +0530

    adding widgets to Material App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './riddles_list.dart';
 
 void main() => runApp(RiddlesApp());
 
@@ -10,8 +11,42 @@ class RiddlesApp extends StatefulWidget {
 }
 
 class _RiddlesAppState extends State<RiddlesApp> {
+  List<Map<String, Object>> _listOfRiddles = RiddlesList().getRiddles();
+  var _riddlesIndex = 0;
+  var _totalScore = 0;
+
+  void _restartRiddlesQuiz() {
+    setState(() {
+      _riddlesIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void answerTheRiddle(int flag) {
+    if (flag == 1) {
+      _totalScore += 1;
+    }
+    _riddlesIndex += 1;
+    if (_riddlesIndex < _listOfRiddles.length) {
+      print("There is a next riddle!");
+    } else {
+      print("There are no more riddles!");
+    }
+  }
+
+  Widget _createRiddle() {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Riddles!"),
+        ),
+        body: _createRiddle(),
+      ),
+    );
   }
 }
